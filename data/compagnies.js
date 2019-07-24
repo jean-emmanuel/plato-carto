12fbb0977e05f45f5568ede73ae44734f25d28c2
bordun <jean-emmanuel.doucet@groolot.net>
2019-07-24 12:08:44 +0200

    add 2019_bis, remove duplicates

--- data/compagnies.js
+++ data/compagnies.js
@@ -1,4 +1,5 @@
 module.exports = []
     .concat(require('./2018/compagnies'))
     .concat(require('./2019/compagnies'))
+    .concat(require('./2019_bis/compagnies'))
     .concat(require('./2019_nodetail/compagnies').map((x)=>{x.reseaux="";x.nodetail=true;return x}))
--- data/structures.js
+++ data/structures.js
@@ -1,4 +1,5 @@
 module.exports = []
     .concat(require('./2018/structures'))
     .concat(require('./2019/structures'))
+    .concat(require('./2019_bis/structures'))
     .concat(require('./2019_nodetail/structures').map((x)=>{x.reseaux="";x.nodetail=true;;return x}))
